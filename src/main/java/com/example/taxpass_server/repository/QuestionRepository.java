8b048d716e8914347ffabe4cca4f2939f75c6fd0
sohyeon-jeon <wjsthgus511@naver.com>
2025-05-22 01:19:18 +0900

    chore: choice_type = 'table'인 경우 circled number(①~⑤) 기준 정렬로 변경

--- src/main/java/com/example/taxpass_server/repository/QuestionRepository.java
+++ src/main/java/com/example/taxpass_server/repository/QuestionRepository.java
@@ -8,45 +8,53 @@ import java.util.List;
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
     @Query(value = """
-            SELECT
-                                                    q.id,
-                                                    q.problem_group_id,
-                                                    q."number",
-                                                    q.exam_year,
-                                                    q.exam_name,
-                                                    q.description,
-                                                   json_agg(
-                                                      json_build_object(
-                                                        'choice_index', c.choice_index,
-                                                        'type', c.choice_type,
-                                                        'content',
-                                                        CASE
-                                                          WHEN c.choice_type = 'table' THEN c.content::jsonb
-                                                          ELSE to_jsonb(c.content)
-                                                        END
-                                                      )
-                                                      ORDER BY\s
-                                                        CASE\s
-                                                          WHEN c.choice_type = 'table'\s
-                                                          THEN regexp_replace(c.content::jsonb ->> '번호', '[^\\d]', '', 'g')::int
-                                                          ELSE c.choice_index
-                                                        END
-                                                    ) AS choices,
-                                                    a.correct_answer,
-                                                    a.explanation
-                                                  FROM questions q
-                                                  JOIN choices c ON q.id = c.question_id
-                                                  JOIN answers a ON q.id = a.question_id
-                                                  GROUP BY
-                                                    q.id,
-                                                    q.problem_group_id,
-                                                    q."number",
-                                                    q.exam_year,
-                                                    q.exam_name,
-                                                    q.description,
-                                                    a.correct_answer,
-                                                    a.explanation
-                                                  ORDER BY q.id;
-        """, nativeQuery = true)
+    SELECT
+      q.id,
+      q.problem_group_id,
+      q."number",
+      q.exam_year,
+      q.exam_name,
+      q.description,
+      json_agg(
+        json_build_object(
+          'choice_index', c.choice_index,
+          'type', c.choice_type,
+          'content',
+          CASE
+            WHEN c.choice_type = 'table' THEN c.content::jsonb
+            ELSE to_jsonb(c.content)
+          END
+        )
+        ORDER BY
+          CASE
+            WHEN c.choice_type = 'table' THEN
+              CASE c.content::jsonb ->> 'number'
+                WHEN '①' THEN 1
+                WHEN '②' THEN 2
+                WHEN '③' THEN 3
+                WHEN '④' THEN 4
+                WHEN '⑤' THEN 5
+                ELSE 999
+              END
+            ELSE c.choice_index
+          END
+      ) AS choices,
+      a.correct_answer,
+      a.explanation
+    FROM questions q
+    JOIN choices c ON q.id = c.question_id
+    JOIN answers a ON q.id = a.question_id
+    GROUP BY
+      q.id,
+      q.problem_group_id,
+      q."number",
+      q.exam_year,
+      q.exam_name,
+      q.description,
+      a.correct_answer,
+      a.explanation
+    ORDER BY q.id
+    """, nativeQuery = true)
+
     List<Object[]> findQuestionBundle();
 }
